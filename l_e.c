7c0875f206a0bed945ca5f9dadbb0d79f592c7cf
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-02-25 22:09:13 +0100

    capacity has a problem

--- l_e.c
+++ l_e.c
@@ -9,14 +9,25 @@ typedef struct IntElement{
 } IntElement;
 
 IntElement *create_IE(int a);//creates an element 
-void push_IE(IntElement *base, IntElement *new);//places new element on top of base element
-IntElement *create_list(int a);//creates a list of a-elements and returns a pointer to the element on top of the list
+void push_IE(IntElement *last, IntElement *new);// links new element to end of the list
+IntElement *create_list(int a);//creates a list of a-elements and returns a pointer to the last element
 int capacity (IntElement *ptr);
 
-int main(){
+int main(){  
 
     IntElement *list_5 = create_list(5);
-    printf("%d\n", capacity(list_5));
+    printf("list_5 %p\n", list_5);
+    int count = 0;
+    while(list_5 != NULL){
+        count++;
+        list_5 = list_5->previous;
+    }
+
+    int c = capacity(list_5);
+    printf("count %d\n", count);
+    printf("capacity %d\n", c);
+
+
     
     return 0;
 }
@@ -30,28 +41,29 @@ IntElement *create_IE(int a){
     return ptr;
 }
 
-void push_IE(IntElement *base, IntElement *new){
-    new->next = base;
-    base->previous = new;
+void push_IE(IntElement *last, IntElement *new){
+    last->next = new;
+    new->previous = last;
      
 }
 
 IntElement *create_list(int a){
-    IntElement *ptrBase = create_IE(0);
+    IntElement *ptrLast = create_IE(0);
     for (int i=0; i<a-1; i++){
         IntElement *ptrNew = create_IE(0);
-        push_IE(ptrBase, ptrNew);
-        ptrBase = ptrNew;
+        push_IE(ptrLast, ptrNew);
+        ptrLast = ptrNew;
     }
 
-    return ptrBase;
+    return ptrLast;
 }
 
 int capacity (IntElement *ptr){
     IntElement *ptrA = ptr;
+    printf("ptr argument capacity %p\n", ptr);
     int number = 0;
     while(ptrA != NULL){
-        number++;
+        number++;         
         ptrA = ptrA->previous;
     }
     return number;
