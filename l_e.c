3ef6d1199e152ca0a74d11f3baebe037384a1787
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-02-25 16:26:56 +0100

    push new element

--- l_e.c
+++ l_e.c
@@ -8,12 +8,19 @@ typedef struct IntElement{
     struct IntElement *next;
 } IntElement;
 
-IntElement *create_first_IE(int a);//creates the first elements 
+IntElement *create_first_IE(int a);//creates the first element 
+void push_IE(IntElement *base, IntElement *new);//places new element in front of base element
 
 int main(){
 
     IntElement *x = create_first_IE(5);
     printf("%d\n", x->data);
+    IntElement *x1 = create_first_IE(77);
+    printf("%d\n", x1->data);
+    push_IE(x,x1);
+    printf("%p\n", x);
+    printf("%p\n", x1->next);
+
 
     return 0;
 }
@@ -25,4 +32,10 @@ IntElement *create_first_IE(int a){
     ptr->next = NULL;
     ptr->previous = NULL;
     return ptr;
+}
+
+void push_IE(IntElement *base, IntElement *new){
+    new->next = base;
+    base->previous = new;
+     
 }
