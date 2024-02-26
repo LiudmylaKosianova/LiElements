f4cc714837205a4ebd864461ebc212809ceb69fa
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-02-26 08:41:14 +0100

    set element fixed

--- l_e.c
+++ l_e.c
@@ -4,6 +4,7 @@
 
 typedef struct IntElement{
     struct IntElement *previous;
+    int index;
     int data;
     struct IntElement *next;
 } IntElement;
@@ -13,23 +14,31 @@ void push_IE(IntElement *last, IntElement *new);// links new element to the end 
 IntElement *create_list(int a);//creates a list of a-elements and returns a pointer to the last element
 int capacity (IntElement *ptr);//returns number of elements in the list
 IntElement *pop(IntElement *list, int a);
+void set_IE(IntElement *list, int index, int a);
 
 
 int main(){  
 
     IntElement *list_5 = create_list(5);
-
-    IntElement *ptrCount = list_5;
+    set_IE(list_5,3,99);       
+    
+    IntElement *ptr1 = list_5;
+    
     int count = 0;
-    while(ptrCount != NULL){
+    while(ptr1 != NULL){
+         
+        if(ptr1->data == 99){
+            printf("found 99\n");
+            break;
+        }
         count++;
-        ptrCount = ptrCount->previous;
+        ptr1 = ptr1->previous;
     }
 
-    int c = capacity(list_5);
-    printf("count %d\n", count);
-    printf("capacity %d\n", c);
-    
+    IntElement *ptr2 = pop(list_5, 99);
+
+    printf("ptr1 %p\n", ptr1);
+    printf("ptr2 %p\n", ptr2);   
     
     return 0;
 }
@@ -38,12 +47,14 @@ IntElement *create_IE(int a){
     IntElement *ptr;
     ptr = (IntElement*)calloc(1,sizeof(IntElement));
     ptr->data = a;
+    ptr->index = 0;
     ptr->next = NULL;
     ptr->previous = NULL;
     return ptr;
 }
 
 void push_IE(IntElement *last, IntElement *new){
+    new->index = last->index + 1;
     last->next = new;
     new->previous = last;
      
@@ -76,11 +87,25 @@ IntElement *pop(IntElement *list, int a){
     IntElement *found = list;
     while(found != NULL){
         if(found->data == a){
+             
             return found;
         }
-        found = found->next;
+        found = found->previous;
     }
     return found;
+
+}
+
+void set_IE(IntElement *list, int index, int a){
+    IntElement *ptrA = list;
+ 
+    while(ptrA != NULL){
+        if(ptrA->index == index){
+            ptrA->data = a;
+            break;
+        }
+        ptrA = ptrA->previous;
+    }
 
 }
 
